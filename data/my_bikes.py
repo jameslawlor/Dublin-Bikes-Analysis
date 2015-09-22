41a9534f7c56cc9285610c3fd9a9ecd071e5028d
James Lawlor <jalawlor@tcd.ie>
2015-09-22 16:08:16 +0100

    Changed SQL DB formatting

--- data/my_bikes.py
+++ data/my_bikes.py
@@ -10,35 +10,43 @@ import os
 
 def getStationData(dt):
     """
-    Scrapes Dublin Bikes JSON station data, returns number of bikes, free stations and current time as a pandas dataframe
+    Scrapes Dublin Bikes JSON station data, returns free bikes at each station
+    (column) with time as row index in a pandas dataframe
     """
     #grabs the current info for all Dublin Bikes from citybikes API
-    json_data = requests.get("http://api.citybik.es/dublinbikes.json") 
-    decoder = json.JSONDecoder()
-    #decode the JSON data into a pandas dataframe structure
-    station_data = pd.DataFrame(decoder.decode(json_data.content))               
-    station_data['time'] = dt
- 
-    return station_data[['bikes','free','time']]
+    station_data =  pd.read_json("http://api.citybik.es/dublinbikes.json")
+    # Set columns to names
+    station_data = station_data.set_index('name')
+    # Transpose data frame
+    station_data =  station_data[['bikes']].transpose()
+    # Add time as row index name
+    station_data.index = [dt]
+    # Formats the station names (columns) appropriately for pandas
+    station_data.columns = [col.replace(" ","_") for col in station_data]
+    return station_data
 
 def getWeatherData(dt):
     """
     Scrapes weather.com for "Dublin's current in pandas dataframe
     """
     weather_result = pywapi.get_weather_from_weather_com("EIXX0014") 
-    return pd.DataFrame([dt, str(weather_result['current_conditions']['text']), \
-                             str(weather_result['current_conditions']['temperature'])])
+    return pd.DataFrame([[dt,
+                            str(weather_result['current_conditions']['text']), \
+                            str(weather_result['current_conditions']['temperature'])]], \
+                        columns=['Time','Weather','Temperature'])
 
-def fileWrite(date, bikes, weather, form='sqlite'):
+def fileWrite(today, bikes, weather, form='sqlite'):
     """
     Write weather and station data to a CSV or sql db file
     """
     if form == 'CSV':
-        bikes.to_csv('bikes_' + date + '.csv', header=False, mode="a")
-        weather.to_csv('weather_' + date + '.csv', header=False, mode="a", index = False)
+        bikes.to_csv('bikes_'+today+'.csv', header=False, mode="a")
+        weather.to_csv('weather_'+today+'.csv', header=False, mode="a", \
+                                                 index = False)
     elif form == 'sqlite':
-        con = sqlite3.connect(date+'_bikes_and_weather.db')
+        con = sqlite3.connect(today+'_bikes_and_weather.db')
         bikes.to_sql('bikes', con, if_exists='append')
+#        print weather
         weather.to_sql('weather', con, if_exists='append', index = False)
     else:
         print 'Incompatible file type' 
@@ -48,19 +56,17 @@ if __name__ == "__main__":
     while True:
         try:
             # Define global datetime of scraping 
-            now = time.strftime("%Y-%m-%d %H:%M:%S") 
+            theDate = time.strftime("%Y-%m-%d")
+            theTime = time.strftime("%H:%M:%S") 
             # Date
-            todays_date = now.split()[0]             
     
-            df_bikes = getStationData(now)
-            df_weather = getWeatherData(now)
+            df_bikes = getStationData(theTime)
+            df_weather = getWeatherData(theTime)
+     
+            fileWrite(theDate,df_bikes,df_weather)
+            print "Data scraped successfully at " + theTime
+
+        except Exception as e: print(e)
         
-            fileWrite(now,df_bikes,df_weather)
-
-            print "Data scraped successfully at " + now
-
-        except:
-            print "Connection Error, retrying in 120s"
-            pass
-
         time.sleep(120)
+
